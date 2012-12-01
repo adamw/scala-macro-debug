f6147e401221ee05ab47a503f4648842567c220a
adamw <adam@warski.org>
2012-12-01 13:24:41 +0100

    Nicer examples

--- examples/src/main/scala/com/softwaremill/debug/DebugExample.scala
+++ examples/src/main/scala/com/softwaremill/debug/DebugExample.scala
@@ -3,25 +3,37 @@ package com.softwaremill.debug
 object DebugExample extends App {
   import DebugMacros._
 
+  header("Hello World macro")
   hello()
 
+  header("Printparam macro")
   val x = 12
   printparam(x)
   printparam(x*2)
   printparam(f(x))
 
-  val z = 10
-  debug1(z)
-  debug1(z*2)
-  debug1(f(z))
+  header("Debug 1 macro")
+  val y = 10
+  debug1(y)
+  debug1(y*2)
+  debug1(f(y))
 
   def test() {
     val p = 11
     debug1(p)
-    debug1(p + z)
+    debug1(p + y)
   }
 
   test()
 
+  // Helper methods
+
   def f(p: Int) = p + 1
+
+  def header(name: String) {
+    println()
+    println("------------")
+    println(name)
+    println("------------")
+  }
 }
