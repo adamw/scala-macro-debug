aa4b09325abb781d03fb2fa73129467359d8d0b2
adamw <adam@warski.org>
2013-03-06 12:23:51 +0100

    Sonatype OSS deployment

--- project/Build.scala
+++ project/Build.scala
@@ -6,17 +6,32 @@ object BuildSettings {
     organization  := "com.softwaremill.scalamacrodebug",
     version       := "0.0.1-SNAPSHOT",
     scalaVersion  := "2.10.0",
-    scalacOptions += "",
-    licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
-    publishTo     <<= (version) { version: String =>
-      val nexus = "https://nexus.softwaremill.com/content/repositories/"
-      if (version.trim.endsWith("SNAPSHOT"))  Some("softwaremill-public-snapshots" at nexus + "snapshots/")
-      else                                    Some("softwaremill-public-releases"  at nexus + "releases/")
+    // Sonatype OSS deployment
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
     credentials   += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     publishMavenStyle := true,
-    // For some reason generating the docs generates a publish error
-    publishArtifact in (Compile, packageDoc) := false
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra := (
+      <scm>
+        <url>git@github.com:adamw/scala-macro-debug.git</url>
+        <connection>scm:git:git@github.com:adamw/scala-macro-debug.git</connection>
+      </scm>
+        <developers>
+          <developer>
+            <id>adamw</id>
+            <name>Adam Warski</name>
+            <url>http://www.warski.org</url>
+          </developer>
+        </developers>),
+    licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
+    homepage      := Some(new java.net.URL("http://www.softwaremill.com"))
   )
 }
 
